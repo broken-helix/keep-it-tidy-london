9ef46d617c2c32e8525315dbfc5556c76298f2f1
James Evans <james_e75@hotmail.com>
2023-06-21 17:48:39 +0000

    Add comment model

--- litter_pickers/models.py
+++ litter_pickers/models.py
@@ -4,7 +4,6 @@ from cloudinary.models import CloudinaryField
 
 
 class Event(models.Model):
-
     title = models.CharField(max_length=200, blank=False)
     details = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
@@ -26,3 +25,17 @@ class Event(models.Model):
     def number_of_attendees(self):
         return self.attending.count()
 
+
+class Comment(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
