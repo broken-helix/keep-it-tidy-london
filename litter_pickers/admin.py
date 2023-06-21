6970f9ced8a23744850cd4610f991cb50d100707
James Evans <james_e75@hotmail.com>
2023-06-21 20:35:36 +0000

    Add prepoulated_fields, search_fields and list_filter to admin panel

--- litter_pickers/admin.py
+++ litter_pickers/admin.py
@@ -4,4 +4,11 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Event)
 class EventAdmin(SummernoteModelAdmin):
-    summernote_fields = ('details')
+    # auto complete slug field in admin panel
+    prepopulated_fields = {'slug': ('date', 'title',)}
+    # add search fields to admin panel
+    search_fields = ('title', 'organiser', 'details',)
+    # add list filter in admin panel
+    list_filter = ('date',)
+    # add wysiwyg editor to admin panel
+    summernote_fields = ('details',)
