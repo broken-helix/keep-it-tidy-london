9010c2ef21455c2180ae7b9bc616e7e8fdc22b19
James Evans <james_e75@hotmail.com>
2023-06-21 17:36:17 +0000

    Add Event model

--- litter_pickers/models.py
+++ litter_pickers/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Event(models.Model):
+
+    title = models.CharField(max_length=200, blank=False)
+    details = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    borough = models.CharField(max_length=80)
+    meeting_point = models.CharField(max_length=200)
+    organiser = models.ForeignKey(User, on_delete=models.CASCADE, related_name='event_organiser')
+    date = models.DateField(blank=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(unique=True)
+    attending = models.ManyToManyField(User, related_name='event_attendees', blank=True)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
+
+    def number_of_attendees(self):
+        return self.attending.count()
+
