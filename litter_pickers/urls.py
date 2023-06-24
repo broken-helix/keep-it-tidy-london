40a98a74272ecf2ec878cae8ac7d581614dd0c4f
James Evans <james_e75@hotmail.com>
2023-06-24 01:58:31 +0000

    Add comment form and ability to attend event

--- litter_pickers/urls.py
+++ litter_pickers/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('events/', views.EventList.as_view(), name='events'),
     path('<slug:slug>/', views.EventDetail.as_view(), name='event_detail'),
+    path('attending/<slug:slug>', views.EventAttending.as_view(), name='event_attending')
 ]
--- litter_pickers/views.py
+++ litter_pickers/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
+from django.http import HttpResponseRedirect
 from .models import Event
 from .forms import CommentForm
 
@@ -25,7 +26,7 @@ class EventDetail(View):
         comments = event.comments.filter(approved=True).order_by("-created")
         attending = False
         if event.attending.filter(id=self.request.user.id).exists():
-            liked = True
+            attending = True
 
         return render(
             request,
@@ -46,7 +47,7 @@ class EventDetail(View):
         comments = event.comments.filter(approved=True).order_by("-created")
         attending = False
         if event.attending.filter(id=self.request.user.id).exists():
-            liked = True
+            attending = True
 
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
@@ -55,6 +56,7 @@ class EventDetail(View):
             comment = comment_form.save(commit=False)
             comment.event = event
             comment.save()
+            comment_form = CommentForm()
         else:
             comment_form = CommentForm()
 
@@ -66,6 +68,19 @@ class EventDetail(View):
                 "comments": comments,
                 "commented": True,
                 "comment_form": comment_form,
-                "attending": attending
+                "attending": attending,
             },
         )
+
+
+class EventAttending(View):
+
+    def post(self, request, slug):
+        event = get_object_or_404(Event, slug=slug)
+        if request.method == "POST" and request.user.is_authenticated:
+            if event.attending.filter(id=request.user.id).exists():
+                event.attending.remove(request.user)
+            else:
+                event.attending.add(request.user)
+
+            return HttpResponseRedirect(reverse('event_detail', args=[slug]))
